5b4e81e68f961a4eeb8e40a00164797da88db73b
Jorge Luis Olarte Vargas <jolarte8811@gmail.com>
2021-11-19 09:07:10 -0500

    Initial commit

---
+++ app/src/main/java/com/jolarte/coco/registro_actividad_activity.kt
@@ -0,0 +1,11 @@
+package com.jolarte.coco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registro_actividad_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_actividad)
+    }
+}
